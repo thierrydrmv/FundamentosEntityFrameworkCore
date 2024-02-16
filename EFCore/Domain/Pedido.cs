54e6d382b52a93912a70444245a81a00cd3954c8
Thierry Varela <varelathierry@gmail.com>
2024-02-16 16:59:55 -0300

    feat: code first

---
+++ EFCore/Domain/Pedido.cs
@@ -0,0 +1,20 @@
+﻿using EFCore.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCore.Domain
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public int ClienteId { get; set; }
+        public Cliente Cliente { get; set;}
+        public DateTime IniciadoEm { get; set; }
+        public DateTime FinalizadoEm { get; set; }
+        public ETipoFrete TipoFrete { get; set; }
+        public EStatusPedido StatusPedido { get; set; }
+        public string Observacao { get; set; }
+        public ICollection<PedidoItem> Itens { get; set; }
+    }
+}
---
+++ EFCore/ValueObjects/EStatusPedido.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCore.ValueObjects
+{
+    public enum EStatusPedido
+    {
+        Analise,
+        Finalizado,
+        Entregue,
+    }
+}
